f073c4749526ebf76ce383b6629e676cdb42ddf7
Renato Filho <renatosousafilho@gmail.com>
2021-04-13 15:16:05 -0300

    logando usuario atual

---
+++ TODO.md
@@ -0,0 +1,11 @@
+# Setup
+* Instalar o socket.io-client
+* Criar o arquivo utils/socketClient
+
+
+# Montar um formulário para o usuário se logar - Ajustar no backend
+
+# Setar o usuário logado para aparecer no avatar
+
+# 
+
--- src/pages/Chat/index.js
+++ src/pages/Chat/index.js
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './styles.css';
 
 import Sidebar from './Sidebar';
 import Body from './Body';
+import socketClient from '../../utils/socketClient';
 
 function Chat() {
-  const user = {
+  const initialValue = {
     id: 1234,
     avatar: '',
     name: 'Washington Campos',
   };
 
+  const [user, setUser] = useState(initialValue);
+
+  useEffect(() => {
+    socketClient.on('chat.currentUser', (user) => {
+      setUser(user);
+    })
+  }, []);
+
   return (
     <div className="chat">
-      <Sidebar />
+      <Sidebar user={user} />
       <Body user={user} />
     </div>
   );
